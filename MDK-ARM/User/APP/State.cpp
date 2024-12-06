95299077d7153f6945e35e281d4ebd58d65ee7e8
YALDZJC <799323497@qq.com>
2024-12-06 13:41:35 +0800

    refactor(MDK-ARM): 优化代码并添加数据采集功能
    - 修改编译优化级别为 4
    - 添加测量数据数组和电压转电流函数
    - 更新 PID 控制逻辑
    - 调整 CAN 通信设置
    - 优化数据发送逻辑

--- MDK-ARM/User/APP/State.cpp
+++ MDK-ARM/User/APP/State.cpp
@@ -4,6 +4,20 @@
 #include "dr16.hpp"
 
 int a;
+float measure_cmd[300];
+float measure_speed[300];
+float measure_curent[300];
+
+float Vol2Current(float x)
+{
+    //    return 0.0112 * x -2.0547;
+    // return 0.4362 * x + 28.7583;
+
+    float p3 = 0.7431;
+    float p4 = -160.397;
+
+    return p3 * x + p4;
+}
 
 // 将期望值做滤波后传入轮子
 void ChassisState::Tar_Updata()
@@ -43,11 +57,15 @@ void ChassisState::Wheel_UpData()
     Chassis_Data.getMinPos[1] = Tools.MinPosHelm(Chassis_Data.tar_angle[1] + Chassis_angle_Init_0x206, Motor6020.GetEquipData(L_Back_6020_ID, Dji_Angle), &Chassis_Data.tar_speed[1], 16384, 8192);
     Chassis_Data.getMinPos[2] = Tools.MinPosHelm(Chassis_Data.tar_angle[2] + Chassis_angle_Init_0x207, Motor6020.GetEquipData(R_Back_6020_ID, Dji_Angle), &Chassis_Data.tar_speed[2], 16384, 8192);
     Chassis_Data.getMinPos[3] = Tools.MinPosHelm(Chassis_Data.tar_angle[3] + Chassis_angle_Init_0x208, Motor6020.GetEquipData(R_Forward_6020_ID, Dji_Angle), &Chassis_Data.tar_speed[3], 16384, 8192);
-    ms++;
+
     if (is_sin == true)
+    {
         sin_t = 4096 + HAL::sinf(2 * 3.1415926 * ms * 0.001 * hz) * 4000;
+        ms++;
+    }
     else
         sin_t = Chassis_Data.getMinPos[0];
+
     // 过零处理
     Chassis_Data.Zero_cross[0] = Tools.Zero_crossing_processing(sin_t, Motor6020.GetEquipData(L_Forward_6020_ID, Dji_Angle), 8192);
     Chassis_Data.Zero_cross[1] = Tools.Zero_crossing_processing(Chassis_Data.getMinPos[1], Motor6020.GetEquipData(L_Back_6020_ID, Dji_Angle), 8192);
@@ -73,6 +91,8 @@ float kp, kd;
 float p_out, D_out;
 float Error;
 float POWER;
+int i;
+
 void ChassisState::PID_Updata()
 {
     if (dr16.ISDir())
@@ -85,7 +105,7 @@ void ChassisState::PID_Updata()
         Chassis_Data.getMinPos[3] = Chassis_angle_Init_0x208;
     }
 
-		feed_6020_1.UpData(Chassis_Data.Zero_cross[0]);
+    feed_6020_1.UpData(Chassis_Data.Zero_cross[0]);
     feed_6020_2.UpData(Chassis_Data.Zero_cross[1]);
     feed_6020_3.UpData(Chassis_Data.Zero_cross[2]);
     feed_6020_4.UpData(Chassis_Data.Zero_cross[3]);
@@ -111,27 +131,36 @@ void ChassisState::PID_Updata()
     pid_vel_0x204.GetPidPos(Kpid_3508_vel, -Chassis_Data.tar_speed[3], td_3508_4.x1, 16384.0f);
 
     Chassis_Data.final_6020_Out[0] = pid_vel_0x205.pid.cout;
-    Chassis_Data.final_6020_Out[1] = pid_vel_0x206.pid.cout;
-    Chassis_Data.final_6020_Out[2] = pid_vel_0x207.pid.cout;
-    Chassis_Data.final_6020_Out[3] = pid_vel_0x208.pid.cout;
+    //    Chassis_Data.final_6020_Out[1] = pid_vel_0x206.pid.cout;
+    //    Chassis_Data.final_6020_Out[2] = pid_vel_0x207.pid.cout;
+    //    Chassis_Data.final_6020_Out[3] = pid_vel_0x208.pid.cout;
 
-    Chassis_Data.final_3508_Out[0] = pid_vel_0x201.pid.cout;
-    Chassis_Data.final_3508_Out[1] = pid_vel_0x202.pid.cout;
-    Chassis_Data.final_3508_Out[2] = pid_vel_0x203.pid.cout;
-    Chassis_Data.final_3508_Out[3] = pid_vel_0x204.pid.cout;
+    //    Chassis_Data.final_3508_Out[0] = pid_vel_0x201.pid.cout;
+    //    Chassis_Data.final_3508_Out[1] = pid_vel_0x202.pid.cout;
+    //    Chassis_Data.final_3508_Out[2] = pid_vel_0x203.pid.cout;
+    //    Chassis_Data.final_3508_Out[3] = pid_vel_0x204.pid.cout;
+
+    if (ms % 100 == 0 && is_sin == true && ms < 300000)
+    {
+        measure_cmd[i] = Chassis_Data.final_6020_Out[0];
+        measure_speed[i] = Motor6020.GetEquipData(L_Forward_6020_ID, Dji_Speed);
+        measure_curent[i] = Motor6020.GetEquipData(L_Forward_6020_ID, Dji_Torque);
+
+        i++;
+    }
 }
 
 void ChassisState::CAN_Setting()
 {
     Motor6020.setMSD(&msd_6020, Chassis_Data.final_6020_Out[0], Get_MOTOR_SET_ID_6020(0x205));
-    Motor6020.setMSD(&msd_6020, Chassis_Data.final_6020_Out[1], Get_MOTOR_SET_ID_6020(0x206));
-    Motor6020.setMSD(&msd_6020, Chassis_Data.final_6020_Out[2], Get_MOTOR_SET_ID_6020(0x207));
-    Motor6020.setMSD(&msd_6020, Chassis_Data.final_6020_Out[3], Get_MOTOR_SET_ID_6020(0x208));
+    //    Motor6020.setMSD(&msd_6020, Chassis_Data.final_6020_Out[1], Get_MOTOR_SET_ID_6020(0x206));
+    //    Motor6020.setMSD(&msd_6020, Chassis_Data.final_6020_Out[2], Get_MOTOR_SET_ID_6020(0x207));
+    //    Motor6020.setMSD(&msd_6020, Chassis_Data.final_6020_Out[3], Get_MOTOR_SET_ID_6020(0x208));
 
-    Motor3508.setMSD(&msd_3508_2006, Chassis_Data.final_3508_Out[0], Get_MOTOR_SET_ID_3508(0x201));
-    Motor3508.setMSD(&msd_3508_2006, Chassis_Data.final_3508_Out[1], Get_MOTOR_SET_ID_3508(0x202));
-    Motor3508.setMSD(&msd_3508_2006, Chassis_Data.final_3508_Out[2], Get_MOTOR_SET_ID_3508(0x203));
-    Motor3508.setMSD(&msd_3508_2006, Chassis_Data.final_3508_Out[3], Get_MOTOR_SET_ID_3508(0x204));
+    //    Motor3508.setMSD(&msd_3508_2006, Chassis_Data.final_3508_Out[0], Get_MOTOR_SET_ID_3508(0x201));
+    //    Motor3508.setMSD(&msd_3508_2006, Chassis_Data.final_3508_Out[1], Get_MOTOR_SET_ID_3508(0x202));
+    //    Motor3508.setMSD(&msd_3508_2006, Chassis_Data.final_3508_Out[2], Get_MOTOR_SET_ID_3508(0x203));
+    //    Motor3508.setMSD(&msd_3508_2006, Chassis_Data.final_3508_Out[3], Get_MOTOR_SET_ID_3508(0x204));
 }
 
 void ChassisState::CAN_Send()
@@ -139,7 +168,7 @@ void ChassisState::CAN_Send()
     // 发送数据
     if (Send_ms == 0)
     {
-        Motor3508.Send_CAN_MAILBOX0(&msd_3508_2006, SEND_MOTOR_ID_3508);
+        //        Motor3508.Send_CAN_MAILBOX0(&msd_3508_2006, SEND_MOTOR_ID_3508);
     }
     else if (Send_ms == 1)
     {
@@ -149,9 +178,9 @@ void ChassisState::CAN_Send()
     Send_ms++;
     Send_ms %= 2;
 
-    Tools.vofaSend(Motor3508.GetEquipData(0x201, Dji_Speed),
-                   Chassis_Data.tar_speed[0],
-                   td_3508_1.x1,
+    Tools.vofaSend(Vol2Current(Chassis_Data.final_6020_Out[0]),
+                   Motor6020.GetEquipData(L_Forward_6020_ID, Dji_Torque),
+                   Motor6020.GetEquipData(L_Forward_6020_ID, Dji_Speed),
                    PowerControl.PowerData.Wheel_Power[2],
                    PowerControl.PowerData.Wheel_Power[3],
                    POWER);
@@ -174,7 +203,7 @@ void Follow_mode::upData()
 
 void Rotating_mode::upData()
 {
-	    Base_UpData();
+    Base_UpData();
 
     // Tar_Updata();
 
